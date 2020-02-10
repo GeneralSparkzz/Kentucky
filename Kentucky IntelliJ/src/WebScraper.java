1c6d96ebaf6e7c5920eb000ab6e56387782e774d
Coby <cobyfrye411@gmail.com>
2020-02-10 10:07:02 -0500

    Finished WebScraper
    Many optimizations, information hiding, and more. WebScraper is finished!
    
    Code cleanup and optimizations.

--- Kentucky IntelliJ/src/WebScraper.java
+++ Kentucky IntelliJ/src/WebScraper.java
@@ -11,8 +11,9 @@ import java.util.Scanner;
 
 public class WebScraper {
 
-    static String usdot, names, address, reason, date, status, newDate, legalName, dbName, street, city, state, zip, insertSQL;
-    static int nlocation, commalocation, lastspace, rowCounter = 0;
+    static String unseparated, usdot, names, address, reason, date, status, newDate, legalName, dbName, street, city, state, zip,
+            insertSQL, hostName, database, user, password;
+    static int linelocation, percentlocation, hashtaglocation, nlocation, commalocation, lastspace, rowCounter = 0;
     static WebDriver driver;
 
     public static void main(String[] args) throws IOException, SQLException {
@@ -21,16 +22,18 @@ public class WebScraper {
     }
 
     public static void insertIntoDatabase() throws IOException, SQLException {
-        String hostName = "ptcwork.database.windows.net";
-        String database = "ChameleonCompanies";
-        String user;
-        String password;
         File file = new File("C:\\Users\\cobyf\\Desktop\\Kentucky.txt");
         BufferedReader br = new BufferedReader(new FileReader(file));
-        String unseparated = br.readLine();
+        unseparated = br.readLine();
         br.close();
-        user = unseparated.substring(0, unseparated.indexOf(","));
-        password = unseparated.substring(unseparated.indexOf(",") + 1);
+        linelocation = unseparated.indexOf("|");
+        percentlocation = unseparated.indexOf("%");
+        hashtaglocation = unseparated.indexOf("#");
+        hostName = unseparated.substring(0, linelocation);
+        database = unseparated.substring(linelocation + 1, percentlocation);
+        user = unseparated.substring(percentlocation + 1, hashtaglocation);
+        password = unseparated.substring(hashtaglocation + 1);
+
         String url = String.format("jdbc:sqlserver://%s:1433;database=%s;user=%s;password=%s;encrypt=true;"
                 + "hostNameInCertificate=*.database.windows.net;loginTimeout=30;", hostName, database, user, password);
         Connection connection = DriverManager.getConnection(url);
@@ -50,6 +53,8 @@ public class WebScraper {
 
             String[] dateInfo = date.split("-");
             newDate = dateInfo[2] + "/" + dateInfo[0] + "/" + dateInfo[1];
+            // System.out.println("Old Date: " + date + " | New Date: " + newDate);
+
             if(names.contains("\n")) {
                 legalName = names.substring(0, names.indexOf("\n"));
                 dbName = names.substring(names.indexOf("\n") + 1);
@@ -57,7 +62,13 @@ public class WebScraper {
                 legalName = names;
                 dbName = "";
             }
+            if(names.contains("'") || address.contains("'")) {
+                legalName = legalName.replaceAll("'", "");
+                dbName = dbName.replaceAll("'", "");
+                address = address.replaceAll("'", "");
+            }
             // System.out.println("Legal Name: " + legalName + "\nDBA Name: " + dbName);
+
             nlocation = address.indexOf("\n");
             commalocation = address.indexOf(",");
             lastspace = address.lastIndexOf(" ");
@@ -70,54 +81,22 @@ public class WebScraper {
             try {
                 insertSQL = String.format("exec SP_Add_Inactive '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s';",
                         usdot, legalName, dbName, street, city, state, zip, reason, newDate, status);
-                statement.executeQuery(insertSQL);
+                statement.executeUpdate(insertSQL);
                 System.out.println(insertSQL + " was inserted.");
-                connection.close();
+                rowCounter++;
             }
-            catch (Exception e) {
+            catch (Exception ex) {
+                System.out.println("Annoyance: " + ex);
                 System.out.println(insertSQL + " was not inserted.");
-                System.out.println("Exception: " + e);
             }
-            rowCounter++;
         }
+
         System.out.println("\n\nDatabase Insertions complete!\n" + rowCounter + " rows inserted.");
-    }
-
-    public static void displayWebsiteInfo() {
-        driver.get("https://li-public.fmcsa.dot.gov/LIVIEW/pkg_oos_process.prc_list?pv_vpath=LIVIEW&" +
-                "pv_show_all=N&pn_dotno=&pn_docket=&pv_legalname=&s_state=KYUS");
-        List<WebElement> rows = driver.findElements(By.xpath("/html/body/font/table[2]/tbody/tr"));
-        for(int x = 2; x <= rows.size(); x++)
-        {
-            WebElement usdot = driver.findElement(By.xpath("/html/body/font/table[2]/tbody/tr[" + x + "]/th/center/font"));
-            WebElement names = driver.findElement(By.xpath("/html/body/font/table[2]/tbody/tr[" + x + "]/td[1]/center"));
-            WebElement address = driver.findElement(By.xpath("/html/body/font/table[2]/tbody/tr[" + x + "]/td[2]/center"));
-            WebElement oosReason = driver.findElement(By.xpath("/html/body/font/table[2]/tbody/tr[" + x + "]/td[3]/center/font"));
-            WebElement oosDate = driver.findElement(By.xpath("/html/body/font/table[2]/tbody/tr[" + x + "]/td[4]/center/font"));
-            WebElement status = driver.findElement(By.xpath("/html/body/font/table[2]/tbody/tr[" + x + "]/td[5]/center/font"));
-
-            String newNames = names.getText().trim().replace("\n", ", ");
-
-            String newAddress = address.getText().trim();
-            newAddress = newAddress.replace("\n", ", "
-            ).replace(" ,", ","
-            ).replace("KY", "KY,");
-
-            String newDate = oosDate.getText().trim();
-            String[] dateInfo = newDate.split("-");
-            newDate = dateInfo[2] + "-" + dateInfo[0] + "-" + dateInfo[1];
-
-            System.out.println("USDOT#: " + usdot.getText().trim());
-            System.out.println("LEGAL NAME, DBA NAME: " + newNames);
-            System.out.println("ADDRESS: " + newAddress);
-            System.out.println("OOS REASON: " + oosReason.getText().toUpperCase());
-            System.out.println("OOS DATE: " + newDate);
-            System.out.println("STATUS: " + status.getText() + "\n");
-        }
+        connection.close();
     }
 
     public static void Start() {
-        System.setProperty("webdriver.chrome.driver", "D:\\College\\!JAR Files\\Selenium Files\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "E:\\College\\!JAR Files\\Selenium Files\\ChromeDriver GamingLaptop\\chromedriver.exe");
         ChromeOptions options = new ChromeOptions();
         options.addArguments("headless");
         options.addArguments("window-size=1920x1080");
